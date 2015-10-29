0bd42f326161478adb9401f0c06c5a57ca592ea6
Leo Kim <leo.kim@atmel.com>
2015-10-29 11:58:36 +0900

    staging: wilc1000: rename hRemainOnChannel of struct host_if_drv
    This patch renames hRemainOnChannel of struct host_if_drv to remain_on_ch_timer
    to avoid CamelCase naming convention.
    
    Signed-off-by: Leo Kim <leo.kim@atmel.com>
    Signed-off-by: Glen Lee <glen.lee@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/host_interface.h
+++ drivers/staging/wilc1000/host_interface.h
@@ -315,7 +315,7 @@ struct host_if_drv {
 /* timer handlers */
 	struct timer_list scan_timer;
 	struct timer_list connect_timer;
-	struct timer_list hRemainOnChannel;
+	struct timer_list remain_on_ch_timer;
 
 	bool IFC_UP;
 };
